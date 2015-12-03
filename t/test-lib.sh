c96adb0fcdbce300277ec501077f0ad63e3ba3ef
David Turner <dturner@twopensource.com>
2015-12-02 19:35:21 -0500

    refs: tests for lmdb backend
    Add tests for the database backend.
    
    Signed-off-by: David Turner <dturner@twopensource.com>
    Helped-by: Dennis Kaarsemaker <dennis@kaarsemaker.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/test-lib.sh
+++ t/test-lib.sh
@@ -958,6 +958,7 @@ test -z "$NO_PERL" && test_set_prereq PERL
 test -z "$NO_PYTHON" && test_set_prereq PYTHON
 test -n "$USE_LIBPCRE" && test_set_prereq LIBPCRE
 test -z "$NO_GETTEXT" && test_set_prereq GETTEXT
+test -n "$USE_LIBLMDB" && test_set_prereq LMDB
 
 # Can we rely on git's output in the C locale?
 if test -n "$GETTEXT_POISON"
